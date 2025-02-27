c5ebe1049bc872d55a301c87d5cb542f82314ac6
Kylian <kylian.patry@gmail.com>
2025-02-27 17:09:44 +0100

    Retreive groups in GroupController

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -5,13 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Group;
 
-final class GroupController extends AbstractController{
+final class GroupController extends AbstractController
+{
     #[Route('/group', name: 'app_group')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $groups = $entityManager->getRepository(Group::class)->findAll();
+
         return $this->render('group/index.html.twig', [
-            'controller_name' => 'GroupController',
+            'groups' => $groups,
         ]);
     }
+
 }
